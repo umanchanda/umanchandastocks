5ecd5eb2c0614c55ed9166264d6adf98ac7e4f52
Uday Manchanda <uday.manchanda14@gmail.com>
2019-08-24 01:18:18 -0400

    update files

--- app.py
+++ app.py
@@ -268,5 +268,5 @@ for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    # port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", debug=True)
